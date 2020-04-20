56680ebfa8f89846d3bf4e345f9603e8965d39b1
George <msg993@gmail.com>
2020-04-20 20:17:58 +0300

    1st step compliance

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,1 +1,10 @@
-console.log('Hello!');
+#!/usr/bin/env node
+
+import program from 'commander';
+
+
+program
+.description('Compares two configuration files and shows a difference.')
+.version('1.0.0');
+
+program.parse(process.argv);
