0ea84ed3914dead70d63a2c1c179c210c602f228
George <msg993@gmail.com>
2020-05-14 12:18:15 +0300

    Fixes of index.js, etc

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,11 +4,10 @@ import program from 'commander';
 import diff from '../index.js';
 
 program
-.name('gendiff')
-.description('Compares two configuration files and shows a difference.')
-.version('1.0.2')
-.option('-f, --format [type]', 'output format')
-.arguments('<filepath1>, <filepath2>')
-.action(diff)
-.parse(process.argv);
-
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.2')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1>, <filepath2>')
+  .action(diff)
+  .parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -4,28 +4,31 @@ import path from 'path';
 const getFullPath = (filepath) => path.resolve(process.cwd(), filepath);
 
 export default (first, second) => {
-    const firstObject = JSON.parse(fs.readFileSync(getFullPath(first), "utf-8"));
-    const secondObject = JSON.parse(fs.readFileSync(getFullPath(second), "utf-8"));
+  const firstObject = JSON.parse(fs.readFileSync(getFullPath(first), 'utf-8'));
+  const secondObject = JSON.parse(fs.readFileSync(getFullPath(second), 'utf-8'));
 
-    let differencies = '{\n';
-    const keysOf2 = Object.keys(secondObject);
-    for (const [key, value] of Object.entries(firstObject)) {
-        if (keysOf2.includes(key)) {
-            if (value === secondObject[key]) {
-                differencies += `    ${key} : ${value}\n`;
-            } else {
-                differencies += `  + ${key} : ${secondObject[key]}\n`;
-                differencies += `  - ${key} : ${value}\n`;
-            }
-        } else {
-            differencies += `  - ${key} : ${value}\n`;
-        }
-        delete secondObject[key];
+  let differencies = '{\n';
+  const keysOf2 = Object.keys(secondObject);
+
+  Object.entries(firstObject).forEach(([key, value]) => {
+    if (keysOf2.includes(key)) {
+      if (value === secondObject[key]) {
+        differencies += `    ${key} : ${value}\n`;
+      } else {
+        differencies += `  + ${key} : ${secondObject[key]}\n`;
+        differencies += `  - ${key} : ${value}\n`;
+      }
+    } else {
+      differencies += `  - ${key} : ${value}\n`;
     }
-    for (const [key, value] of Object.entries(secondObject)) {
-        differencies += `  + ${key} : ${value}\n`;
-    }
-    differencies += `}`;
-    console.log(differencies);
-    return differencies;
+    delete secondObject[key];
+  });
+
+  Object.entries(secondObject).forEach(([key, value]) => {
+    differencies += `  + ${key} : ${value}\n`;
+  });
+
+  differencies += '}';
+  console.log(differencies);
+  return differencies;
 };
