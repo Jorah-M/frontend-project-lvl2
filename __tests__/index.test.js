8ee0137ce24b45948fc4c8aa594680dd25f89aca
George <msg993@gmail.com>
2020-05-14 14:08:13 +0300

    Test for yaml flat files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,11 +6,20 @@ import fs from 'fs';
 const __filename = fileURLToPath(import.meta.url); 
 const __dirname = path.dirname(__filename);
 
-test('flat files diff', () => {
+test('flat json files diff', () => {
     const path1 = path.resolve(__dirname, 'fixtures/before.json');
     const path2 = path.resolve(__dirname, 'fixtures/after.json');
-    const testDiff = diff(path1, path2);
+    const testDiff1 = diff(path1, path2);
 
     const expectedFlatOutput = fs.readFileSync('__tests__/fixtures/expectFlat.txt', 'utf-8').trim();
-    expect (testDiff).toBe(expectedFlatOutput);
+    expect (testDiff1).toBe(expectedFlatOutput);
+})
+
+test('flat yaml files diff', () => {
+    const path3 = path.resolve(__dirname, 'fixtures/before.yml');
+    const path4 = path.resolve(__dirname, 'fixtures/after.yml');
+    const testDiff2 = diff(path3, path4);
+
+    const expectedFlatOutput = fs.readFileSync('__tests__/fixtures/expectFlat.txt', 'utf-8').trim();
+    expect (testDiff2).toBe(expectedFlatOutput);
 })
